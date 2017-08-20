0c74eeb574829da7c0052e1a40f8411ce70c9ff1
Archit Chhikara <Archit.Chhikara@Cerner.com>
2017-08-20 20:48:51 +0530

    ReactDOMServer in play

--- server.js
+++ server.js
@@ -16,12 +16,16 @@ server.use(sassMiddleWare({
 
 server.set('view engine', 'ejs');
 
-import './serverRender';
+import serverRender from './serverRender';
 
 server.get('/', (req, res) => {
-  res.render('index', {
-    content: '...'
-  });
+  serverRender()
+    .then(content => {
+      res.render('index', {
+        content
+      });
+    })
+    .catch(console.error);
 });
 
 server.use('/api', apiRouter);
@@ -30,40 +34,3 @@ server.use(express.static('public'));
 server.listen(config.port, config.host, () => {
   console.info('Express is ears on port ', config.port);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-// Creating server with http module
-// import http from 'http';
-// const server = http.createServer();
-// server.listen(8080);
-// server.on('request', (req, res) => {
-//   res.write('Hello Sirji!\n');
-//   setTimeout(() => {
-//     res.write('This is how I hold');
-//     res.end;
-//   }, 3000);
-// });
-
-// Testing the basic imports
-// import config, {nodeEnv} from './config'; 
-// console.log(config, nodeEnv);
-
-// Playing with the http or https
-// import http from 'http';
-// http.get('http://www.geeksforgeeks.org/data-structures/', res => {
-//   console.log('Response status code: ', res.statusCode);
-//   // console.log('Response: ', res.connection);
-//   res.on('data', chunk => {
-//     console.log(chunk.toString());
-//   });
-// });
--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 import Header from './Header';
 import ContestPreview from './ContestPreview';
 
-class App extends React.Component{
+class App extends React.Component {
   state = {
     pageHeader: 'Naming Contests',
-    contests: [] 
+    contests: this.props.initialContests
   };
 
   //Component Lifecycle methods
@@ -24,13 +24,13 @@ class App extends React.Component{
     //clean timers and listeners
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div className='App'>
         <Header message={this.state.pageHeader} />
         <div>
           {this.state.contests.map(contest =>
-            <ContestPreview key ={contest.id} {...contest} />
+            <ContestPreview key={contest.id} {...contest} />
           )}
         </div>
       </div>
